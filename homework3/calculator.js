8ff4564ee5934b78a22f5ca718396f7842bfa94b
throw-Exceptions <ehdtls901@gmail.com>
2020-10-13 17:21:11 +0900

    [과제 4] DOM 조작을 jQuery로 하도록 변경

--- homework3/calculator.js
+++ homework3/calculator.js
@@ -1,120 +1,89 @@
 let power = false;
 let formula = "";
 let inputNumber = "";
-let element = document.getElementById("onoff");
-element.addEventListener("click", function() {
+let formulaDisplay = $("#formula");
+let mainDisplay = $("#input");
+let onOffButton = $("#onoff");
+
+$("#onoff").on("click", function() {
     if (power === true) {
         power = false;
-        let e = document.getElementById("input");
-        e.textContent = "NOT POWERED...";
-        e = document.getElementById("onoff");
-        e.textContent = "ON";
-        e = document.getElementById("formula");
-        e.textContent = "formula";
+        formulaDisplay.text("formula");
+        mainDisplay.text("NOT POWERED...");
+        onOffButton.text("ON");
     } else {
         power = true;
-        let e = document.getElementById("input");
-        e.textContent = "0";
-        e = document.getElementById("onoff");
-        e.textContent = "OFF";
-        e = document.getElementById("formula");
-        e.textContent = "";
+        formulaDisplay.text("");
+        mainDisplay.text("0");
+        onOffButton.text("OFF");
         formula = "";
     }
 });
 
-element = document.getElementById("equal");
-element.addEventListener("click", function() {
-    if (power === true) {
-        formula += inputNumber;
-        inputNumber = "";
-        let e = document.getElementById("formula");
-        e.textContent = formula;
-        let value = eval(formula);
-        e = document.getElementById("input");
-        if (value != undefined) {
-            e.textContent = value;
-        } else {
-            e.textContent = "formula is abnormal";
-        }
+$("#equal").on("click", function() {
+    if (power != true) return;
+    formula += inputNumber;
+    inputNumber = "";
+    formulaDisplay.text(formula);
+    let value = eval(formula);
+    if (value != undefined) {
+        mainDisplay.text(value);
+    } else {
+        mainDisplay.text("formula is abnormal");
     }
 });
 
-element = document.getElementsByClassName("number");
-for(let i = 0; i < element.length; i++) {
-    let text = element[i].textContent;
-    element[i].addEventListener("click", function() {
-        if (power === true) {
-            inputNumber += text;
-            let e = document.getElementById("input");
-            e.textContent = inputNumber;
-        }
-    });
-}
+$(".number").on("click", function() {
+    if (power != true) return;
+    let num = $(this).text()
+    inputNumber += num;
+    mainDisplay.text(inputNumber);
+});
 
-element = document.getElementsByClassName("operator");
-for(let i = 0; i < element.length; i++) {
-    let text = element[i].textContent;
-    element[i].addEventListener("click", function() {
-        if (power === true) {
-            formula += inputNumber;
-            if (text === "x") {
-                formula += "*";
-            } else {
-                formula += text;
-            }
-            inputNumber = "";
-            let e = document.getElementById("formula");
-            e.textContent = formula;
-            e = document.getElementById("input");
-            e.textContent = "0";
-        }
-    });
-}
+$(".operator").on("click", function() {
+    if (power != true) return;
+    let oper = $(this).text();
+    formula += inputNumber;
+    if (oper === "x") {
+        formula += "*";
+    } else {
+        formula += oper;
+    }
+    inputNumber = "";
+    formulaDisplay.text(formula);
+    mainDisplay.text("0");
+});
 
-element = document.getElementById("erase");
-element.addEventListener("click", function() {
-    if (power === true) {
-        let e = document.getElementById("input");
-        e.textContent = "0";
-        inputNumber = "";
+$("#erase").on("click", function() {
+    if (power != true) return;
+    mainDisplay.text("0");
+    inputNumber = "";
+});
+
+$("#clean").on("click", function() {
+    if (power != true) return;
+    mainDisplay.text("0")
+    inputNumber = "";
+    formulaDisplay.text("");
+    formula = "";
+});
+
+$("#delete").on("click", function() {
+    if (power != true) return;
+    inputNumber = inputNumber.substring(0, inputNumber.length - 1);
+    if (inputNumber.length === 0) {
+        mainDisplay.text("0");
+    } else {
+        mainDisplay.text(inputNumber);
     }
 });
 
-element = document.getElementById("clean");
-element.addEventListener("click", function() {
-    if (power === true) {
-        let e = document.getElementById("input");
-        e.textContent = "0";
-        inputNumber = "";
-        e = document.getElementById("formula");
-        e.textContent = "";
-        formula = "";
+$("#unary").on("click", function() {
+    if (power != true) return;
+    if (inputNumber[0] === "-") {
+        inputNumber = inputNumber.substring(1);
+    } else {
+        inputNumber = "-" + inputNumber;
     }
-});
-
-element = document.getElementById("delete");
-element.addEventListener("click", function() {
-    if (power === true) {
-        inputNumber = inputNumber.substring(0, inputNumber.length - 1);
-        let e = document.getElementById("input");
-        if (inputNumber.length === 0) {
-            e.textContent = "0";
-        } else {
-            e.textContent = inputNumber;
-        }
-    }
-});
-
-element = document.getElementById("unary");
-element.addEventListener("click", function() {
-    if (power === true) {
-        if (inputNumber[0] === "-") {
-            inputNumber = inputNumber.substring(1);
-        } else {
-            inputNumber = "-" + inputNumber;
-        }
-        let e = document.getElementById("input");
-        e.textContent = inputNumber;
-    }
+    mainDisplay.text(inputNumber);
 });
